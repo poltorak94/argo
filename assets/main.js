78d1c9d80a0ae74d6c13663823d8ec3696f06474
epol94 <elisey1005@gmail.com>
2023-05-24 10:54:22 +0300

    argo

---
+++ assets/main.js
@@ -0,0 +1,141 @@
+$(function () {
+  new WOW().init();
+});
+$(document).ready(function () {
+  //main slider
+  const heroSwiper = new Swiper(".hero__swiper", {
+    loop: true,
+    pagination: {
+      clickable: true,
+      el: ".hero .slider-pagination__items",
+    },
+  });
+
+  //слайдер проектов
+  const projectsSwiper = new Swiper(".projects__slider", {
+    loop: true,
+    pagination: {
+      clickable: true,
+      el: ".projects .swiper__pagination",
+    },
+  });
+
+  //слайдер товаров
+  const productsSwiper = new Swiper(".production__swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".production__right",
+      prevEl: ".production__left",
+    },
+  });
+
+  //слайдер клиентов
+  const clientsSwiper = new Swiper(".clients__swiper", {
+    // loop: true,
+    // slidesPerView: "auto",
+    // spaceBetween: 48,
+    // freeMode: true,
+    // speed: 2000,
+    // autoplay: {
+    //   delay: 0,
+    //   disableOnInteraction: false,
+    // },
+
+    observer: true,
+    observeParents: true,
+    spaceBetween: 48,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    loop: true,
+    speed: 2500,
+    autoplay: {
+      delay: 10,
+      disableOnInteraction: false,
+    },
+  });
+
+  //счетчик
+  let countsBox = $(".about").offset().top;
+
+  //хедер
+  let header = $(".header");
+  let headerHeight = header.height() + 20;
+  if ($(window).scrollTop() >= headerHeight) {
+    header.addClass("header--scroll");
+  }
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= countsBox / 2) {
+      $(".about__advantages-item-value").each(function () {
+        $(this)
+          .prop("Counter", 0)
+          .animate(
+            {
+              Counter: $(this).attr("data-num"),
+            },
+            {
+              duration: 3000,
+              easing: "swing",
+              step: function (now) {
+                $(this).text(Math.ceil(now).toLocaleString());
+              },
+            }
+          );
+      });
+    }
+
+    if ($(window).scrollTop() >= headerHeight) {
+      header.addClass("header--scroll");
+    } else {
+      header.removeClass("header--scroll");
+    }
+  });
+
+  $(".header__nav-item--dropdown").on("mouseenter", function () {
+    let dropDown = $(this).find(".header__dropdown-menu");
+    dropDown.fadeIn("fast");
+  });
+
+  $(".header__nav-item--dropdown").on("mouseleave", function () {
+    let dropDown = $(this).find(".header__dropdown-menu");
+    dropDown.fadeOut("fast");
+  });
+
+  //точки на карте
+  $(".map__item").each(function () {
+    let xPos = parseFloat($(this).attr("data-x"));
+    let yPos = parseFloat($(this).attr("data-y"));
+
+    let xPos2 = parseFloat($(this).attr("data-x-2"));
+    let yPos2 = parseFloat($(this).attr("data-y-2"));
+    $(this).css("top", yPos + "px");
+    $(this).css("left", xPos + "px");
+  });
+
+  //modal
+  $(".header__nav-callback, .production__order, .mob-block__callback").on(
+    "click",
+    function (e) {
+      e.preventDefault();
+      $("body").trigger("open.modal");
+    }
+  );
+  $("body").on("open.modal", function () {
+    $(".modal").removeClass("modal--hidden");
+    $(".modal").arcticmodal({
+      afterClose: function (data, el) {
+        $(".modal").addClass("modal--hidden");
+      },
+    });
+  });
+
+  //Маска телефон
+  $(".input-tel").mask("+7 (999) 999-99-99");
+
+  //mobile menu
+  $(".header__burger").on("click", function (e) {
+    e.preventDefault();
+    $(this).closest(".header").toggleClass("header--opened");
+    $(this).closest(".header").addClass("header--scroll");
+    $(".mob-overlay").fadeToggle();
+  });
+});
